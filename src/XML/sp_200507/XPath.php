1c9d47c5bedd27b1ea44dd4e11b65047a037e5b4
Tim van Dijen <tvdijen@gmail.com>
2026-06-19 21:23:36 +0200

    Raise coverage

--- src/XML/sp_200507/XPath.php
+++ src/XML/sp_200507/XPath.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace SimpleSAML\WebServices\SecurityPolicy\XML\sp_200507;
 
-use Dom;
-use SimpleSAML\WebServices\SecurityPolicy\Assert\Assert;
-use SimpleSAML\XML\DOMDocumentFactory;
 use SimpleSAML\XML\TypedTextContentTrait;
-use SimpleSAML\XMLSchema\Type\StringValue;
+use SimpleSAML\XMLSchema\Type\Helper\XPathValue;
 
 /**
  * An XPath element
@@ -20,18 +17,5 @@ final class XPath extends AbstractSpElement
     use TypedTextContentTrait;
 
 
-    public const string TEXTCONTENT_TYPE = StringValue::class;
-
-
-    /**
-     * Validate the content of the element.
-     *
-     * @throws \SimpleSAML\XMLSchema\Exception\SchemaViolationException on failure
-     */
-    protected function validateContent(string $content): void
-    {
-        $dom = new Dom\XPath(DOMDocumentFactory::create());
-        $result = $dom->evaluate($content);
-        Assert::isInstanceOf($result, Dom\NodeList::class);
-    }
+    public const string TEXTCONTENT_TYPE = XPathValue::class;
 }
